246ef5a30c7f45b8c5d6f265ae93f9f200db1470
Julia Winchester <Julia.Winchester@stonybrook.edu>
2017-04-23 18:59:10 -0400

    Implementing Angular routes and sub-pages

---
+++ app.js
@@ -0,0 +1,24 @@
+angular.module('ApotropaApp', ['ngMaterial', 'ngRoute'])
+
+.controller('MainController', function($scope) {})
+
+.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/code', {
+			templateUrl: 'code/code.html',
+			controller: 'MainController'
+		})
+		.when('/publication', {
+			templateUrl: 'publication/publication.html',
+			controller: 'MainController'
+		})
+		.when('/teaching', {
+			templateUrl: 'teaching/teaching.html',
+			controller: 'MainController'
+		})
+		.otherwise({
+			templateUrl: 'home.html',
+			controller: 'MainController'
+		});
+});
+
